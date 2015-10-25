9c20066408bed8826c40e511db805aad1cba1834
rick94 <rick94sm@gmail.com>
2015-10-25 17:32:55 -0600

    Inicio Interfaz Casos de Prueba

---
+++ SistemaPruebas/Intefaces/InterfazCasosDePrueba.aspx
@@ -0,0 +1,129 @@
+﻿<%@ Page EnableEventValidation="false" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="InterfazCasosDePrueba.aspx.cs" Inherits="SistemaPruebas.Intefaces.CasosDePrueba" Async="true"  %>
+<asp:Content ID="Content1" ContentPlaceHolderID="MainContent" runat="server">
+
+<h2><%: Title %>Casos de Prueba</h2>
+<div class="form-group">
+    <div class="col-md-offset-10 col-md-12">
+        <asp:Button runat="server" Text="Insertar" CssClass="btn btn-default" ID="BotonCPInsertar" OnClick="BotonCPInsertar_Click"/>
+
+        <asp:Button runat="server" Text="Modificar" CssClass="btn btn-default" ID="BotonCPModificar" />
+
+        <asp:Button runat="server" Text="   Eliminar" CssClass="btn btn-default" ID="BotonCPEliminar"  OnClientClick="return confirm('¿Está seguro que desea eliminar este caso de prueba?')" />
+    </div>
+</div>
+
+
+<div class ="row">
+    <div class ="col-md-8">
+        <div class="form-horizontal">
+           <div class="form-group">      
+                <asp:Label ID="ProyectoCP" CssClass="col-md-2 control-label" runat="server" Text="Proyecto:"></asp:Label>    
+                <div class="col-md-10">
+                    <asp:DropDownList ID="ProyectoComboBox" runat="server"></asp:DropDownList>
+               </div>
+           </div>
+           <div class="form-group">      
+                <asp:Label ID="DisenoCP" runat="server" CssClass="col-md-2 control-label" Text="Diseño:"></asp:Label>    
+                <div class="col-md-10">
+                    <asp:DropDownList ID="DisenoComboBox" runat="server"></asp:DropDownList>
+               </div>
+           </div>
+           <div class="form-group">      
+                <asp:Label ID="RequerimientoCP" runat="server" CssClass="col-md-2 control-label" Text="Requerimiento:"></asp:Label>    
+                <div class="col-md-10">
+                    <asp:DropDownList ID="RequerimientoComboBox" runat="server"></asp:DropDownList>
+               </div>
+           </div>
+           <div class="form-group">      
+                <asp:Label ID="PropositoCP" runat="server" CssClass="col-md-2 control-label" Text="Propósito:"></asp:Label>    
+                <div class="col-md-10">
+                    <asp:TextBox runat="server" ID="TextBoxPropositoCP" style="width:250px;height:50px" CssClass="form-control" MaxLength="50" TextMode="multiline"/>
+                </div>
+           </div>
+           <div class="form-group">      
+                <asp:Label ID="ResultadoCP" runat="server" CssClass="col-md-2 control-label" Text="Resultado esperado:"></asp:Label>    
+                <div class="col-md-8">
+                    <asp:TextBox runat="server" ID="TextBoxResultadoCP" style="width:250px;height:50px" CssClass="form-control" MaxLength="50" TextMode="multiline"/>
+               </div>
+           </div>
+        </div>
+    </div>
+
+    <div class="col-md-4">
+         <div class="form-horizontal">
+            <h4>Entrada de Datos</h4>
+            <div class ="borderCP" >
+                <div class="form-group">      
+                    <asp:Label ID="EntradaDatosCP" runat="server" CssClass="col-md-2 control-label" Text="Entrada de Datos:"></asp:Label>    
+                    <div class="col-md-8">
+                        <asp:TextBox runat="server" ID="TextBoxEntradaDatos" style="width:250px" CssClass="form-control"/>
+                    </div>
+                </div>
+                <div class="form-group">          
+                    <div class="col-md-8">
+                         <asp:DropDownList ID="TipoEntrada" runat="server"></asp:DropDownList>
+                    </div>
+                </div>
+                <div class="form-group">          
+                    <div class="col-md-8">
+                          <div class="col-md-offset-10 col-md-12">
+                                <asp:Button runat="server" Text="Agregar"                               
+                                    CssClass="btn btn-default" ID="AgregarEntrada" />
+                                <asp:Button runat="server" Text="Eliminar"  
+                                    CssClass="btn btn-default" ID="EliminarEntrada"/>
+                          </div>
+                    </div>
+                </div>
+                <div class="form-group">          
+                    <div class="col-md-8">
+                        <asp:GridView ID="DECP" runat ="server" margin-right ="auto" 
+                                    CellPadding="10" 
+                                    margin-left="auto" 
+                                    CssClass ="GridView" HorizontalAlign="Center"  
+                                    HeaderStyle-BackColor="#eeeeee" HeaderStyle-ForeColor="#333333" BorderColor="#CDCDCD" border-radius="15px" 
+                                    AutoPostBack ="true" AutoGenerateColumns ="False">                            
+                        </asp:GridView>
+                    </div>
+                </div>
+                <div class="form-group">      
+                    <asp:Label ID="FlujoCP" runat="server" CssClass="col-md-2 control-label" Text="Flujo Central:"></asp:Label>    
+                        <div class="col-md-8">
+                            <asp:TextBox runat="server" ID="TextBoxFlujoCentral" style="width:250px;height:50px" CssClass="form-control" MaxLength="50" TextMode="multiline"/>
+                        </div>
+                </div>
+
+
+                <div style="clear:both"></div>
+            </div>
+        </div>
+    </div>
+
+</div>
+
+
+<div class="form-group">
+    <div class="col-md-offset-10 col-md-12">
+        <asp:Button runat="server" style="border-color:#4bb648;color:#4bb648"
+            Text="Aceptar" 
+            causesvalidation="true"                              
+            CssClass="btn btn-default" 
+            ID="BotonCPAceptar" 
+        />
+        <asp:Button runat="server" Text="Cancelar" style="border-color:#fe6c4f;color:#fe5e3e" 
+            CssClass="btn btn-default" ID="BotonCPCancelar"  
+            OnClientClick="return confirm('¿Está seguro que desea cancelar?')" 
+        />
+    </div>
+</div>
+
+ <div class="row">        
+    <asp:GridView ID="CP" runat ="server" margin-right ="auto" 
+            CellPadding="10" 
+            margin-left="auto" 
+            CssClass ="GridView" HorizontalAlign="Center" 
+            AllowPaging="true"   PageSize="5" 
+            HeaderStyle-BackColor="#eeeeee" HeaderStyle-ForeColor="#333333" BorderColor="#cdcdcd" border-radius="15px" 
+            AutoPostBack ="true" >      
+    </asp:GridView>
+</div> 
+</asp:Content>
---
+++ SistemaPruebas/Intefaces/InterfazCasosDePrueba.aspx.designer.cs
@@ -0,0 +1,231 @@
+﻿//------------------------------------------------------------------------------
+// <generado automáticamente>
+//     Este código fue generado por una herramienta.
+//
+//     Los cambios en este archivo podrían causar un comportamiento incorrecto y se perderán si
+//     se vuelve a generar el código. 
+// </generado automáticamente>
+//------------------------------------------------------------------------------
+
+namespace SistemaPruebas.Intefaces {
+    
+    
+    public partial class CasosDePrueba {
+        
+        /// <summary>
+        /// Control BotonCPInsertar.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button BotonCPInsertar;
+        
+        /// <summary>
+        /// Control BotonCPModificar.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button BotonCPModificar;
+        
+        /// <summary>
+        /// Control BotonCPEliminar.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button BotonCPEliminar;
+        
+        /// <summary>
+        /// Control ProyectoCP.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label ProyectoCP;
+        
+        /// <summary>
+        /// Control ProyectoComboBox.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.DropDownList ProyectoComboBox;
+        
+        /// <summary>
+        /// Control DisenoCP.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label DisenoCP;
+        
+        /// <summary>
+        /// Control DisenoComboBox.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.DropDownList DisenoComboBox;
+        
+        /// <summary>
+        /// Control RequerimientoCP.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label RequerimientoCP;
+        
+        /// <summary>
+        /// Control RequerimientoComboBox.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.DropDownList RequerimientoComboBox;
+        
+        /// <summary>
+        /// Control PropositoCP.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label PropositoCP;
+        
+        /// <summary>
+        /// Control TextBoxPropositoCP.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox TextBoxPropositoCP;
+        
+        /// <summary>
+        /// Control ResultadoCP.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label ResultadoCP;
+        
+        /// <summary>
+        /// Control TextBoxResultadoCP.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox TextBoxResultadoCP;
+        
+        /// <summary>
+        /// Control EntradaDatosCP.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label EntradaDatosCP;
+        
+        /// <summary>
+        /// Control TextBoxEntradaDatos.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox TextBoxEntradaDatos;
+        
+        /// <summary>
+        /// Control TipoEntrada.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.DropDownList TipoEntrada;
+        
+        /// <summary>
+        /// Control AgregarEntrada.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button AgregarEntrada;
+        
+        /// <summary>
+        /// Control EliminarEntrada.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button EliminarEntrada;
+        
+        /// <summary>
+        /// Control DECP.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.GridView DECP;
+        
+        /// <summary>
+        /// Control FlujoCP.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label FlujoCP;
+        
+        /// <summary>
+        /// Control TextBoxFlujoCentral.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox TextBoxFlujoCentral;
+        
+        /// <summary>
+        /// Control BotonCPAceptar.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button BotonCPAceptar;
+        
+        /// <summary>
+        /// Control BotonCPCancelar.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button BotonCPCancelar;
+        
+        /// <summary>
+        /// Control CP.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.GridView CP;
+    }
+}
