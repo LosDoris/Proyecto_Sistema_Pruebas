7e6ec880d3f868c8aa3e6bfed0f84ce33ee92ced
andreagomez95 <andreittttta@gmail.com>
2015-10-25 12:38:03 -0600

    cont Insertar controladora

--- SistemaPruebas/ControladorasBD/ControladoraBDDisenno.cs
+++ SistemaPruebas/ControladorasBD/ControladoraBDDisenno.cs
@@ -10,26 +10,26 @@ namespace SistemaPruebas.Controladoras
     {
         Acceso.Acceso acceso = new Acceso.Acceso();
         /*
-         * Requiere: Entidad de Recursos Humanos
-         * Modifica: Inserta un nuevo recurso humano en el sistema.
+         * Requiere: Entidad de Disenno
+         * Modifica: Inserta un nuevo disenno en el sistema.
          * Retorna: int.
          */
         public int insertarDisennoBD(EntidadDisenno Disenno)
         {
-            //String consulta = "INSERT INTO Recurso_Humano(cedula, nombre_completo, telefono1, telefono2, correo_electronico, usuario, contrasenna, perfil_acceso, rol, id_proyecto,fechaUltimo) values(" + Disenno.Cedula + ",'" + Disenno.Nombre_Completo + "','" + Disenno.Tel1 + "','" + Disenno.Tel2 + "','" + Disenno.Correo + "','" + Disenno.Usuario + "','" + Disenno.Clave + "','" + Disenno.PerfilAcceso + "','" + Disenno.Rol + "'," + Disenno.ProyAsociado + ", getDate()" + ")";
-            int ret = acceso.Insertar(""/*consulta*/);
+            String consulta = "INSERT INTO Disenno_Prueba(id_disenno,proposito,nivel,tecnica,tipo,ambiente,procedimiento,fecha_de_disenno,criterio_aceptacion,responsable,id_proyecto,fechaUltimo) values(" + Disenno.Id + ",'" + Disenno.Proposito + "','" + Disenno.Nivel + "','" + Disenno.Tecnica + "','" + Disenno.Tipo + "','" + Disenno.Ambiente + "','" + Disenno.Procedimiento + "','" + Disenno.FechaDeDisenno + "','" + Disenno.CriterioAceptacion + "'," + Disenno.Responsable + "'," + Disenno.ProyAsociado + ", getDate()" + ")";
+            int ret = acceso.Insertar(consulta);
             return ret;
 
         }
 
         /*
-         * Requiere: Entidad de Recursos Humanos
-         * Modifica: Modifica un recurso humano previamente ingresado al sistema.
+         * Requiere: Entidad de Disenno
+         * Modifica: Modifica un disenno previamente ingresado al sistema.
          * Retorna: int.
          */
         public int modificarDisennoBD(EntidadDisenno Disenno)
         {
-            //String consulta = "UPDATE Recurso_Humano SET cedula =" + Disenno.Cedula + ", nombre_completo = '" + Disenno.Nombre_Completo + "', telefono1 = '" + Disenno.Tel1 + "', telefono2 = '" + Disenno.Tel2 + "', correo_electronico = '" + Disenno.Correo + "', usuario = '" + Disenno.Usuario + "', contrasenna = '" + Disenno.Clave + "', perfil_acceso = '" + Disenno.PerfilAcceso + "', rol = '" + Disenno.Rol + "', id_proyecto = '" + Disenno.ProyAsociado + "', fechaUltimo=getDate()" + " WHERE cedula = " + Disenno.CedulaVieja + ";";
+            //String consulta = "UPDATE Disenno_Prueba SET cedula =" + Disenno.Cedula + ", nombre_completo = '" + Disenno.Nombre_Completo + "', telefono1 = '" + Disenno.Tel1 + "', telefono2 = '" + Disenno.Tel2 + "', correo_electronico = '" + Disenno.Correo + "', usuario = '" + Disenno.Usuario + "', contrasenna = '" + Disenno.Clave + "', perfil_acceso = '" + Disenno.PerfilAcceso + "', rol = '" + Disenno.Rol + "', id_proyecto = '" + Disenno.ProyAsociado + "', fechaUltimo=getDate()" + " WHERE cedula = " + Disenno.CedulaVieja + ";";
             int ret = acceso.Insertar(""/*consulta*/);
             return ret;
 
@@ -37,13 +37,13 @@ namespace SistemaPruebas.Controladoras
 
         /*
          * Requiere: Cédula
-         * Modifica: Elimina un recurso humano del sistema.
+         * Modifica: Elimina un disenno del sistema.
          * Retorna: int.
          */
 
         public int eliminarDisennoBD(int cedula)
         {
-            return 0; //acceso.Insertar("DELETE FROM Recurso_Humano WHERE cedula = " + cedula + ";");
+            return 0; //acceso.Insertar("DELETE FROM Disenno_Prueba WHERE cedula = " + cedula + ";");
 
         }
 
@@ -58,11 +58,11 @@ namespace SistemaPruebas.Controladoras
             String consulta = "";
             if (tipo == 1)//consulta para llenar grid, no ocupa la cedula pues los consulta a todos
             {
-                //consulta = "SELECT cedula, nombre_completo, rol, id_proyecto FROM Recurso_Humano ORDER BY fechaUltimo desc;";//BY perfil_acceso";
+                //consulta = "SELECT cedula, nombre_completo, rol, id_proyecto FROM Disenno_Prueba ORDER BY fechaUltimo desc;";//BY perfil_acceso";
             }
             else if (tipo == 2)
             {
-                //consulta = "SELECT cedula, nombre_completo, telefono1, telefono2, correo_electronico, usuario, contrasenna, perfil_acceso, rol, id_proyecto FROM Recurso_Humano WHERE cedula =" + cedula;
+                //consulta = "SELECT cedula, nombre_completo, telefono1, telefono2, correo_electronico, usuario, contrasenna, perfil_acceso, rol, id_proyecto FROM Disenno_Prueba WHERE cedula =" + cedula;
                 // dt = acceso.ejecutarConsultaTabla(consulta);
             }
 
