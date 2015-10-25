feeb65b7983c263110a4652e02a501cf0a4e6078
andreagomez95 <andreittttta@gmail.com>
2015-10-25 13:26:14 -0600

    Entidad requerimiento

---
+++ SistemaPruebas/Entidades/EntidadRequerimientos.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SistemaPruebas.Entidades
+{
+    public class EntidadRequerimiento
+    {
+        private String id;
+        private String precondiciones;
+
+        private String requerimientosEspeciales;
+
+        public EntidadRequerimiento(Object[] datos)
+        {
+            this.id = datos[0].ToString();
+            this.precondiciones = datos[1].ToString();
+            this.requerimientosEspeciales = datos[2].ToString();
+        }
+
+        //Metodos set y get para la variable Id
+        public String Id
+        {
+            get { return id; }
+            set { id = value; }
+        }
+
+        //Metodos set y get para la variable proposito
+        public String Precondiciones
+        {
+            get { return precondiciones; }
+            set { precondiciones = value; }
+        }
+
+        //Metodos set y get para la variable ambiente
+        public String RequerimientosEspeciales
+        {
+            get { return requerimientosEspeciales; }
+            set { requerimientosEspeciales = value; }
+        }
+    }
+}
+
+
