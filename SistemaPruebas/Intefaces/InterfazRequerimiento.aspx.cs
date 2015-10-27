910a299b422463cb85c64020142bf7ba0ec2d6a2
andreagomez95 <andreittttta@gmail.com>
2015-10-27 15:20:20 -0600

    Creacion de clases de Requerimiento

---
+++ SistemaPruebas/Intefaces/InterfazRequerimiento.aspx.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace SistemaPruebas.Intefaces
+{
+    public partial class InterfazRequerimiento : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
