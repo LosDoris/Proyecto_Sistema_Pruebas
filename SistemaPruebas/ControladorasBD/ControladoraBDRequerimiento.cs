1001bd95ec86b84e67b535e12574464174cdfed9
andreagomez95 <andreittttta@gmail.com>
2015-10-30 10:11:24 -0600

    REQ

--- SistemaPruebas/ControladorasBD/ControladoraBDRequerimiento.cs
+++ SistemaPruebas/ControladorasBD/ControladoraBDRequerimiento.cs
@@ -83,7 +83,7 @@ namespace SistemaPruebas.Controladoras
          */
         public int modificarRequerimientoBD(Controladoras.EntidadRequerimientos requerimiento)
         {
-            String consulta = "UPDATE Requerimiento SET id_requerimiento='"+requerimiento.Id+"',precondiciones='"+requerimiento.Precondiciones+"',Requerimientos_especiales='"+requerimiento.RequerimientosEspeciales+"',id_proyecto='"+12+"',fechaUltimo=getDate() WHERE id_requerimiento='"+ANDREA"+';";
+            String consulta = "UPDATE Requerimiento SET id_requerimiento='" + requerimiento.Id + "',precondiciones='" + requerimiento.Precondiciones + "',Requerimientos_especiales='" + requerimiento.RequerimientosEspeciales + "',id_proyecto='" + requerimiento.Proyecto + "',fechaUltimo=getDate() WHERE id_requerimiento='" + requerimiento.IdViejo + "+';";
             // = "UPDATE Recurso_Humano SET cedula =" + requerimiento.Cedula + ", nombre_completo = '" + requerimiento.Nombre_Completo + "', telefono1 = '" + requerimiento.Tel1 + "', telefono2 = '" + requerimiento.Tel2 + "', correo_electronico = '" + requerimiento.Correo + "', usuario = '" + requerimiento.Usuario + "', contrasenna = '" + requerimiento.Clave + "', perfil_acceso = '" + requerimiento.PerfilAcceso + "', rol = '" + requerimiento.Rol + "', id_proyecto = '" + requerimiento.ProyAsociado + "', fechaUltimo=getDate()" + " WHERE cedula = " + requerimiento.CedulaVieja + ";";
             int ret = acceso.Insertar(consulta);
             return ret;
