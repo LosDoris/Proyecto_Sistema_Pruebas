8d3985ce91f18ec5b2f48cd559e79d3aa19839e7
danielperaltamadriz <danielperaltamadriz@gmail.com>
2015-11-05 12:22:35 -0600

    Entidad para manejar los requerimientos que estan asociados a un diseño

---
+++ SistemaPruebas/Entidades/EntidadDisennoReq.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SistemaPruebas.Entidades
+{
+    public class EntidadDisennoReq
+    {
+
+        private int idProyecto;
+        private string idReq;
+        private int idDisenno;
+
+        public EntidadDisennoReq (object []datos)
+        {
+            this.idProyecto = Int32.Parse(datos[0].ToString());
+            this.idReq = datos[1].ToString();
+            this.idDisenno = Int32.Parse(datos[2].ToString());
+        }
+
+
+        public int IdProyecto
+        {
+            get { return idProyecto; }
+            set { idProyecto = value; }
+        }
+        public string IdReq
+        {
+            get { return idReq; }
+            set { idReq = value; }
+        }
+        public int IdDisenno
+        {
+            get { return idDisenno; }
+            set { idDisenno = value; }
+        }
+
+    }
+}
