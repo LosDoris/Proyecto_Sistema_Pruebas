60330bfa2117aab7e49784c9c00a9ccfc81c1d2f
danielperaltamadriz <danielperaltamadriz@gmail.com>
2015-11-17 10:37:03 -0600

    Controladora Reportes

---
+++ SistemaPruebas/Controladoras/ControladoraReportes.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data;
+
+namespace SistemaPruebas.Controladoras
+{
+    public class ControladoraReportes
+    {
+        ControladoraProyecto controlProy;
+        ControladoraDisenno controlDis;
+        ControladoraCasosPrueba controlCasos;
+        ControladoraEjecucionPrueba controlEjec;
+
+        public ControladoraReportes()
+        {
+            controlProy = new ControladoraProyecto();
+            controlDis = new ControladoraDisenno();
+            controlCasos = new ControladoraCasosPrueba();
+            controlEjec = new ControladoraEjecucionPrueba();
+        }
+
+        public DataTable consultarProyecto()
+        {
+            return controlProy.ConsultarProyectoIdNombre();
+        }
+        public EntidadProyecto consultarProyecto(string nombre)
+        {
+            return controlProy.ConsultarProyecto(controlProy.ConsultarIdProyectoPorNombre(nombre));
+        }
+        public DataTable consultarDisennos(string nombre)
+        {
+            return controlDis.consultarDisenoGrid(controlProy.ConsultarIdProyectoPorNombre(nombre));
+        }
+        public EntidadDisenno consultarDisenno(string nombre)
+        {
+            return controlDis.consultarDisenno(controlDis.consultarId_Disenno(nombre));
+            //return null;
+        }
+
+    }
+}
