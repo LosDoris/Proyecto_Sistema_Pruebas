819070b2e5d43df7ecbdb3198dbc11070e3eb7bb
danielperaltamadriz <danielperaltamadriz@gmail.com>
2015-11-17 11:53:07 -0600

    proy

--- SistemaPruebas/Controladoras/ControladoraReportes.cs
+++ SistemaPruebas/Controladoras/ControladoraReportes.cs
@@ -67,7 +67,35 @@ namespace SistemaPruebas.Controladoras
         public int generarReporte(string nombreP, bool[] camposP)
         {
             reporteProyecto(consultarProyecto(nombreP), camposP);
+            Document document = new Document(PageSize.A4, 88f, 88f, 10f, 10f);
+            Font NormalFont = FontFactory.GetFont("Arial", 12, Font.NORMAL, Color.BLACK);
+            using (System.IO.MemoryStream memoryStream = new System.IO.MemoryStream())
+            {
+                PdfWriter writer = PdfWriter.GetInstance(document, memoryStream);
+                Phrase phrase = null;
+                PdfPCell cell = null;
+                PdfPTable table = null;
+                System.Drawing.Color color = null;
 
+                document.Open();
+
+                //Header Table
+                table = new PdfPTable(2);
+                table.TotalWidth = 500f;
+                table.LockedWidth = true;
+                table.SetWidths(new float[] { 0.3f, 0.7f });
+
+
+                //Company Name and Address
+                phrase = new Phrase();
+                phrase.Add(new Chunk("Microsoft Northwind Traders Company\n\n", FontFactory.GetFont("Arial", 16, Font.BOLD, Color.RED)));
+                phrase.Add(new Chunk("107, Park site,\n", FontFactory.GetFont("Arial", 8, Font.NORMAL, Color.BLACK)));
+                phrase.Add(new Chunk("Salt Lake Road,\n", FontFactory.GetFont("Arial", 8, Font.NORMAL, Color.BLACK)));
+                phrase.Add(new Chunk("Seattle, USA", FontFactory.GetFont("Arial", 8, Font.NORMAL, Color.BLACK)));
+                cell = PhraseCell(phrase, PdfPCell.ALIGN_LEFT);
+                cell.VerticalAlignment = PdfCell.ALIGN_TOP;
+                table.AddCell(cell);
+            }
             return 0;
         }
 
