1b1b336c1965f57ce29947f12e82add01168e5c7
danielperaltamadriz <danielperaltamadriz@gmail.com>
2015-11-18 09:30:25 -0600

    reporte

--- SistemaPruebas/Controladoras/ControladoraReportes.cs
+++ SistemaPruebas/Controladoras/ControladoraReportes.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Data;
+using iTextSharp;
 using iTextSharp.text;
 using iTextSharp.text.pdf;
 using System.Data;
@@ -66,36 +67,20 @@ namespace SistemaPruebas.Controladoras
         }
         public int generarReporte(string nombreP, bool[] camposP)
         {
-            //reporteProyecto(consultarProyecto(nombreP), camposP);
-            //Document document = new Document(PageSize.A4, 88f, 88f, 10f, 10f);
-            //Font NormalFont = FontFactory.GetFont("Arial", 12, Font.NORMAL, Color.BLACK);
-            //using (System.IO.MemoryStream memoryStream = new System.IO.MemoryStream())
-            //{
-            //    PdfWriter writer = PdfWriter.GetInstance(document, memoryStream);
-            //    Phrase phrase = null;
-            //    PdfPCell cell = null;
-            //    PdfPTable table = null;
-            //    System.Drawing.Color color = null;
+            Document doc = new Document(PageSize.LETTER);
+            var output = new System.IO.FileStream(Server.MapPath("MyFirstPDF.pdf"), System.IO.FileMode.Create);
+            var writer = PdfWriter.GetInstance(doc, output);
+            doc.Open();
+            var logo = iTextSharp.text.Image.GetInstance(Server.MapPath("~/Images/orderedList5.png"));
+            logo.SetAbsolutePosition(430, 770);
+            logo.ScaleAbsoluteHeight(30);
+            logo.ScaleAbsoluteWidth(70);
+            doc.Add(logo);
+            doc.Add(new Paragraph("Hello World"));
+            doc.Close();
+            //Response.Redirect("~/MyFirstPDF.pdf");           
+            Page.ClientScript.RegisterStartupScript(this.GetType(), "OpenWindow", "window.open('MyFirstPDF.pdf','_newtab');", true);
 
-            //    document.Open();
-
-            //    //Header Table
-            //    table = new PdfPTable(2);
-            //    table.TotalWidth = 500f;
-            //    table.LockedWidth = true;
-            //    table.SetWidths(new float[] { 0.3f, 0.7f });
-
-
-            //    //Company Name and Address
-            //    phrase = new Phrase();
-            //    phrase.Add(new Chunk("Microsoft Northwind Traders Company\n\n", FontFactory.GetFont("Arial", 16, Font.BOLD, Color.RED)));
-            //    phrase.Add(new Chunk("107, Park site,\n", FontFactory.GetFont("Arial", 8, Font.NORMAL, Color.BLACK)));
-            //    phrase.Add(new Chunk("Salt Lake Road,\n", FontFactory.GetFont("Arial", 8, Font.NORMAL, Color.BLACK)));
-            //    phrase.Add(new Chunk("Seattle, USA", FontFactory.GetFont("Arial", 8, Font.NORMAL, Color.BLACK)));
-            //    cell = PhraseCell(phrase, PdfPCell.ALIGN_LEFT);
-            //    cell.VerticalAlignment = PdfCell.ALIGN_TOP;
-            //    table.AddCell(cell);
-            //}
             return 0;
         }
 
