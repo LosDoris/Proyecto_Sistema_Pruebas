8dfde66bcc543a307b190c004c95550b562e9383
danielperaltamadriz <danielperaltamadriz@gmail.com>
2015-11-25 12:50:21 -0600

    metricas

--- SistemaPruebas/Controladoras/ControladoraReportes.cs
+++ SistemaPruebas/Controladoras/ControladoraReportes.cs
@@ -51,12 +51,12 @@ namespace SistemaPruebas.Controladoras
         }
         public String consultarMiembrosProyecto(String nombProy)
         {
-            DataTable dt=controlRH.consultarMiembrosProyecto(nombProy);
-            String miembros="";
+            DataTable dt = controlRH.consultarMiembrosProyecto(nombProy);
+            String miembros = "";
             foreach (DataRow dr in dt.Rows)
             {
-                miembros = miembros+ "\n "+dr[0] + ",\n ";
-               // dtGrid.Rows.Add(datos);
+                miembros = miembros + "\n " + dr[0] + ",\n ";
+                // dtGrid.Rows.Add(datos);
             }
             return miembros;
 
@@ -98,18 +98,18 @@ namespace SistemaPruebas.Controladoras
             return controlReq.consultarReqUnico(nombre, idProyeto);
         }
 
-       
+
 
         public List<object> reporteProyecto(EntidadProyecto entidad)
         {
 
-            
+
 
             List<Object> retorno = new List<object>();
 
-            retorno.Add( entidad.Nombre_sistema);
+            retorno.Add(entidad.Nombre_sistema);
             retorno.Add(entidad.Objetivo_general);
-            retorno.Add(entidad.Fecha_asignacion);           
+            retorno.Add(entidad.Fecha_asignacion);
             string estado = "";
             switch (Int32.Parse(entidad.Estado))
             {
@@ -146,15 +146,15 @@ namespace SistemaPruebas.Controladoras
             retorno.Add(entidad.Nombre_representante);
             retorno.Add(entidad.LiderProyecto);
             retorno.Add(controlRH.solicitarNombreRecursosPorProyecto(Int32.Parse(entidad.Id_proyecto)));
-      
+
             return retorno;
         }
 
-     
+
 
         public List<Object> medicionRequerimiento(List<Object> retorno, string idReq)
         {
-           
+
 
             int exitosCant = 0;
             int sinEnvaluarCant = 0;
@@ -163,41 +163,96 @@ namespace SistemaPruebas.Controladoras
             Dictionary<string, int> noConformidad = new Dictionary<string, int>();
 
             string[] casosPrueba = controlCasos.consultarCasoPorRequerimiento(idReq);
-
-            foreach (string casito in casosPrueba)
+            if (casosPrueba[0] != "")
             {
-                if (controlEjec.retornarEstado(casito) == "Satisfactorio")//Aún no se ha terminado, hay que realizar una consulta en ejecución.//Se supone caso exitoso
+                foreach (string casito in casosPrueba)
                 {
-                    exitosCant++;
-                    idCasosExitosos.Add(casito);
-                }
-                else if (controlEjec.retornarEstado(casito) == "Fallido")//Se supone caso de no conformidad
-                {
-                    string key = "";
-                    if (noConformidad.ContainsKey(key))//Se suma una nueva no conformidad
+                    string[] estado = controlEjec.retornarEstado(casito).Split(';');
+                    if (estado[0] != "")
                     {
-                        noConformidad[key]++;
+                        foreach (string estadito in estado)
+                        {
+
+                            if (estadito.Split(',')[0] == "Satisfactorio")//Aún no se ha terminado, hay que realizar una consulta en ejecución.//Se supone caso exitoso
+                            {
+                                exitosCant++;
+                                idCasosExitosos.Add(casito);
+                            }
+                            else if (estadito.Split(',')[1] == "Fallido")//Se supone caso de no conformidad
+                            {
+                                string key = estadito.Split(',')[0];
+                                if (noConformidad.ContainsKey(key))//Se suma una nueva no conformidad
+                                {
+                                    noConformidad[key]++;
+                                }
+                                else//Se agrega nueva no conformidad
+                                {
+                                    noConformidad.Add(key, 1);
+                                }
+                            }
+
+                        }
                     }
-                    else//Se agrega nueva no conformidad
+                    else//casos de prueba que no han sido evaluados aún
                     {
-                        noConformidad.Add(key, 1);
+                        sinEnvaluarCant++;
+                        idCasosSinEvaluar.Add(casito);
                     }
+
                 }
-                else//casos de prueba que no han sido evaluados aún
+                retorno.Add(idReq);
+                string CasosEx = "Cantidad de casos exitosos: " + exitosCant.ToString() + "\nCasos que son exitosos:";
+                foreach (string rr in idCasosExitosos)
+                    CasosEx += "\n\t" + rr;
+                retorno.Add(CasosEx);
+
+                double[] fallidosInt = new double[noConformidad.Count];
+                double[] fallidosPorc = new double[noConformidad.Count];
+                string[] fallidosString = new string[noConformidad.Count];
+                double total = 0;
+
+                int i = 0;
+                foreach (KeyValuePair<string, int> entry in noConformidad)
                 {
-                    sinEnvaluarCant++;
-                    idCasosSinEvaluar.Add(casito);
+
+                    fallidosInt[i] = entry.Value;
+                    total += entry.Value;
+                    fallidosString[i] = entry.Key;
+                    i++;
                 }
+
+                i = 0;
+                foreach (double d in fallidosInt)
+                {
+                    fallidosPorc[i] = (d / total) * 100;
+                    i++;
+                }
+
+                string noConf = "Tipos de no conformidad:\n";
+                for (int j = 0; j < fallidosInt.Length; j++)
+                {
+                    noConf += "\t" + fallidosString[j] + ": " + fallidosInt[j] + " - " + fallidosPorc[j] + "%.\n";
+                }
+
+                retorno.Add(noConf);
+                string sinEv = "Casos de prueba sin evaluar:";
+                foreach (string sin in idCasosSinEvaluar)
+                {
+                    sinEv += "\n\t" + sin;
+                }
+                sinEv += "\n Para un total de: " + sinEnvaluarCant;
+                retorno.Add(sinEv);
+            }
+            else
+            {
+                retorno.Add("");
+                retorno.Add("");
+                retorno.Add("");
             }
 
-            retorno.Add(idReq);
-            retorno.Add(exitosCant);
-            retorno.Add(idCasosExitosos);
-            retorno.Add(noConformidad);
-            retorno.Add(sinEnvaluarCant);
-            retorno.Add(idCasosSinEvaluar);
-
+            
             return retorno;
+
         }
         /*
             bool[] proyecto = new bool[12];
@@ -214,7 +269,8 @@ namespace SistemaPruebas.Controladoras
             proyecto[10] = CheckBoxTipoNoConf.Checked;
             proyecto[11] = CheckBoxCantNoConf.Checked;
         */
-        public DataTable crearDT(bool [] campos) {
+        public DataTable crearDT(bool[] campos)
+        {
             DataTable dt = new DataTable();
             if (campos[0])
             {
@@ -266,16 +322,18 @@ namespace SistemaPruebas.Controladoras
             }
             return dt;
         }
-        public DataTable dtReporte(bool [] campos, String proy, String mod, String req)
+        public DataTable dtReporte(bool[] campos, String proy, String mod, String req)
         {
             EntidadProyecto entidad = consultarProyecto(proy);
             int k = 0;
-            for (int j = 0; j < 12; ++j) {
-                if (campos[j] == true) {
+            for (int j = 0; j < 12; ++j)
+            {
+                if (campos[j] == true)
+                {
                     k = k + 1;
                 }
             }
-            Object [] datos= new Object[k];
+            Object[] datos = new Object[k];
             /*
             bool[] proyecto = new bool[12];
             proyecto[0] = CheckBoxNombreProyecto.Checked;
@@ -310,38 +368,38 @@ namespace SistemaPruebas.Controladoras
                 datos[i] = mod;
                 i = i + 1;
             }
-            
+
             if (campos[2])
             {
                 datos[i] = req;
                 i = i + 1;
             }
-            
+
             if (campos[3])
             {
                 datos[i] = entidad.Fecha_asignacion;
                 i = i + 1;
             }
-            
+
             if (campos[4])
             {
-                datos[i] = entidad.Oficina_representante+ "\n"+ entidad.Nombre_representante + "\n" + entidad.Telefono_representante;
+                datos[i] = entidad.Oficina_representante + "\n" + entidad.Nombre_representante + "\n" + entidad.Telefono_representante;
                 i = i + 1;
 
             }
-            
+
             if (campos[5])
             {
                 datos[i] = entidad.LiderProyecto;
                 i = i + 1;
             }
-            
+
             if (campos[6])
             {
                 datos[i] = entidad.Objetivo_general;
                 i = i + 1;
             }
-            
+
             if (campos[7])
             {
                 string estado = "";
@@ -382,32 +440,32 @@ namespace SistemaPruebas.Controladoras
                 i = i + 1;
                 //= entidad.Nombre_sistema;
             }
-            
+
             if (campos[8])//miembros
             {
                 String miembros = consultarMiembrosProyecto(proy);
                 datos[i] = miembros;// entidad.Nombre_sistema;
                 i = i + 1;
             }
-            
+
             if (campos[9])//exitos
             {
                 datos[i] = "-";//entidad.Nombre_sistema;
                 i = i + 1;
             }
-            
+
             if (campos[10])//tipo
             {
                 datos[i] = "-";//entidad.Nombre_sistema;
                 i = i + 1;
             }
-            
+
             if (campos[11])//cant no conf
             {
                 datos[i] = "-";//entidad.Nombre_sistema;
                 i = i + 1;
             }
-            
+
             dt.Rows.Add(datos);
 
             return dt;
